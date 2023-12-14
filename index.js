e0eb476c73e79214c23648ab5cda5773e302827e
m-gaia <gaia.marianela@gmail.com>
2023-12-14 19:57:22 -0300

    vercel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require ("path")
 const app = express();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.set("views",path.join(__dirname,"./src/views"))
 app.set("view engine","ejs")
